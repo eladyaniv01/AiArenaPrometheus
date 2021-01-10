33dc474eb902cbc9ad1a560d91c37d7ede702e1c
Elad <eladyaniv01@gmail.com>
2021-01-10 20:05:50 +0200

    add working AC linked to prod

--- config_dev.py
+++ config_dev.py
@@ -2,11 +2,11 @@ import os
 from urllib import parse
 from arenaclient.match.matches import HttpApiMatchSource
 # docker run -it --entrypoint /bin/bash -v D:\proj\aiarena-client\arenaclient\configs\config_dev.py:/root/aiarena-client/config.py --name AC 6266ef93e52b
-ARENA_CLIENT_ID = "arenaclient_SOMENAME"
-API_TOKEN = "SOMETOKEN"
+ARENA_CLIENT_ID = "arenaclient_elad"
+API_TOKEN = "681ba530efaf6e5f28f835688c39e3eb562e52ec"
 
 ROUNDS_PER_RUN = 1
-BASE_WEBSITE_URL = "https://staging.aiarena.net"
+BASE_WEBSITE_URL = "https://aiarena.net"
 API_MATCHES_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/matches/")
 API_RESULTS_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/results/")
 API_SET_STATUS_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/set-status/")
@@ -17,7 +17,6 @@ TEMP_PATH = "/tmp/aiarena/"
 LOCAL_PATH = os.path.dirname(__file__)
 WORKING_DIRECTORY = LOCAL_PATH  # same for now
 LOG_FILE = os.path.join(WORKING_DIRECTORY, "../aiarena-client.log")
-# LOG_FILE = "/root/aiarena-client.log"
 REPLAYS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "../replays")
 BOTS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "../bots")
 CLEAN_BOT_DIRECTORIES_BEFORE_MATCH_START = False
